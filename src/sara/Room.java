9d499d9f56d5ff0db0c3a42a9d15842846e63639
nbart2010 <nbart2010@10.137.60.130>
2014-10-05 12:23:37 -0400

    added variables and few methods to room class

--- src/sara/Room.java
+++ src/sara/Room.java
@@ -6,11 +6,36 @@
 package sara;
 
 import java.io.Serializable;
+import java.util.LinkedList;
 
 /**
  *
  * @author Jeff
  */
 class Room implements Serializable {
+    
+    private int roomID;
+    private int roomCapacity;
+    private static LinkedList<Equipment> equipList = new LinkedList<Equipment>(); 
+    private boolean studentReserveable;
+    private static int nextID=1;
+    
+    public int getID(){return roomID;}
+    public int getRoomCap(){return roomCapacity;}
+    public boolean getStReserveable(){return studentReserveable;}
+    
+    public Room () {
+        roomID = nextID;
+        nextID++;
+    }
+    
+    public Room (int roomCap, Equipment equip, boolean stReserv){
+        this.roomID=nextID;
+        this.roomCapacity = roomCap;
+        equipList.add(equip);
+        this.studentReserveable = stReserv;
+        
+        nextID++;
+    }
     
 }
